81efcfd5cfedf7deb0c3f004e20b7bb62390003f
Mrbysco <Mrbysco@users.noreply.github.com>
2021-07-08 03:29:21 +0200

    Update Agricraft
    Remove unneeded lines

--- src/main/java/com/mrbysco/wasaila/WailaHandler.java
+++ src/main/java/com/mrbysco/wasaila/WailaHandler.java
@@ -1,9 +1,6 @@
 package com.mrbysco.wasaila;
 
-import java.util.List;
-
 import com.infinityraider.agricraft.api.v1.misc.IAgriDisplayable;
-
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
@@ -18,22 +15,13 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 @WailaPlugin
 public class WailaHandler implements IWailaDataProvider, IWailaPlugin {
-	@Override
-	public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
-		return null;
-	}
-	
-	@Override
-	public List<String> getWailaHead(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor,
-			IWailaConfigHandler config) {
-		return tooltip;
-	}
 
     @Override
-    public List<String> getWailaBody(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor,
-    		IWailaConfigHandler config) {
+    public List<String> getWailaBody(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
     	Block block = accessor.getBlock();
         if (block instanceof IAgriDisplayable) {
             ((IAgriDisplayable) block).addDisplayInfo(tooltip::add);
@@ -46,14 +34,7 @@ public class WailaHandler implements IWailaDataProvider, IWailaPlugin {
     }
 
     @Override
-    public List<String> getWailaTail(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor,
-    		IWailaConfigHandler config) {
-    	return tooltip;
-    }
-    
-    @Override
-    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world,
-    		BlockPos pos) {
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
     	if (te != null) {
             te.writeToNBT(tag);
         }
